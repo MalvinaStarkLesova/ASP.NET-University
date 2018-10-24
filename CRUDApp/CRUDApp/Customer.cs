622770561475aa168820ddd4533c7f5123ffaf92
MalvinaLesova <39057901+MalvinaStarkLesova@users.noreply.github.com>
2018-10-24 20:46:50 +0300

    CRUDApp
    First attempt in making a CRUD application (possibly successful)

---
+++ CRUDApp/CRUDApp/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDApp
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(45)]
+        public string Name { get; set; }
+    }
+}
